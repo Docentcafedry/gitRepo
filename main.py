16459445c22a679b7325b18b3b365dff5a110ae9
Lesha <Your Email>
2023-11-22 12:11:15 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def my_func(*, name,  age):
+    print(name, age)
+
+
+
+my_func(name='Lesha', age=2)
+
+def meeting_rooms(rooms: list[list[int]]) -> list[list[int]]:
+    rooms.sort()
+    for i in range(1, len(rooms)):
+        if rooms[i][0] < rooms[i-1][1]:
+            return False
+
+    return True
